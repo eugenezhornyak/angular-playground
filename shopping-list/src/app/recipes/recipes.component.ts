bed1e6e34cea8cad03800e9d50314bb7f0512715
Yevhenii Zhorniak <38474637+eugenezhornyak@users.noreply.github.com>
2020-07-28 21:02:49 +0300

    41 - 43 lessons. Header was created according to 7th bootstrap

---
+++ shopping-list/src/app/recipes/recipes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styles: [
+  ]
+})
+export class RecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
