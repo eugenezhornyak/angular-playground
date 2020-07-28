6f40a2407c8648c3fcda025340533ca818b4052e
Yevhenii Zhorniak <38474637+eugenezhornyak@users.noreply.github.com>
2020-07-28 22:28:57 +0300

    44 - 49 lessons [Add] Recipe model

--- shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A recipe', 'Do smth', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg'),
+    new Recipe('A recipe', 'Do smth', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg')
+  ];
 
   constructor() { }
 
--- shopping-list/src/app/shopping-list/shopping-list.component.ts
+++ shopping-list/src/app/shopping-list/shopping-list.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients = [];
 
   constructor() { }
 
