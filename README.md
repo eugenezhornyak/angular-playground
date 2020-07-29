f01c4642314655e99fabe900d837c81d6a8c9a25
Yevhenii Zhorniak <38474637+eugenezhornyak@users.noreply.github.com>
2020-07-29 19:55:39 +0300

    lesson 65 [Update] Incapsulation mode

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 This repository contains Angular examples.
 
 ## Shopping list
+
+## Servers manager
+Primitive project which demonstrate how components can communicate data and events to each other.
--- servers-manager/src/app/server-element/server-element.component.ts
+++ servers-manager/src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.scss']
+  styleUrls: ['./server-element.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ServerElementComponent implements OnInit {
   @Input('serverElement')
