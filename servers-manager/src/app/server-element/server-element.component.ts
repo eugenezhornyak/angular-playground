5a8da9b002a6a311666080d216ee56fa372edf56
Yevhenii Zhorniak <38474637+eugenezhornyak@users.noreply.github.com>
2020-07-31 14:17:55 +0300

    66 - 73 lessons ViewChild lifecycle hooks

--- servers-manager/src/app/server-element/server-element.component.ts
+++ servers-manager/src/app/server-element/server-element.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges,
+  SimpleChange, SimpleChanges, DoCheck, AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit, OnDestroy,
+  ViewChild, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,13 +9,50 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit,
+                                               AfterContentChecked, AfterViewInit, AfterViewChecked
+                                               , OnDestroy {
   @Input('serverElement')
-  element:{type: string, name: string, content: string};
+  element: {type: string, name: string, content: string};
+  @Input()
+  name: string;
 
-  constructor() { }
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') contentParagraph: ElementRef;
 
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor called!');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called!');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called!');
+    console.log(this.header.nativeElement.textContent);
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called!');
+    console.log(this.header.nativeElement.textContent);
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called!');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called!');
+    console.log(this.contentParagraph.nativeElement.textContent);
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called!');
+    // console.log(this.header.nativeElement.textContent);
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called!');
+  }
 }
